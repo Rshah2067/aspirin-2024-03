b6a6a9cd0904e80b3259b7b62b31d80b4b4b1680
Rohan Shah <rohanshah2067@gmail.com>
2024-11-05 23:35:38 -0500

    Final Commit for assignment

--- assignments/05-concurrency/src/main.rs
+++ assignments/05-concurrency/src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
 use rand::Rng;
+use std::{
+    sync::{Arc, Mutex},
+    time::Instant,
+};
+use thread_pool::ThreadPool;
 
 mod error;
 mod thread_pool;
@@ -12,6 +17,146 @@ fn random_vec(capacity: usize) -> Vec<i64> {
 }
 
 fn main() -> Result<()> {
+    let start = Instant::now();
+    let threadcount = 10;
     let data = random_vec(10_000_000);
+    let pool = ThreadPool::new(threadcount, vec![1], vec![1])?;
+    //split our data so we can head it off to a thread
+    let data_vec = Arc::new(Mutex::new(split_array(data.len() / threadcount, data)));
+    //give a job to each one of the threads
+    for i in 0..threadcount {
+        let data_vec = Arc::clone(&data_vec); // Clone the Arc to pass to each thread
+        pool.execute(move |_| {
+            let data_vec = data_vec.lock().unwrap(); // Lock to access the data safely
+            let chunk = &data_vec[i];
+            sort(chunk.to_vec()) // Sort the chunk in place
+        })?;
+    }
+    //get all of our sorted vectors back
+    let sorted = pool.get_results();
+    //merge all of our sorted arrays;
+    let merged = merge_results(sorted);
+    //now we are going to check wheter we did this correctly
+    let duration = start.elapsed();
+    println!("{:?}", duration);
+    assert!(
+        merged.windows(2).all(|w| w[0] <= w[1]),
+        "Vector is not sorted"
+    );
     Ok(())
+}
+//given the array to be sorted this function splits it into n arrays and passes back
+//a vector of n unsorted arrays
+fn split_array(n: usize, input: Vec<i64>) -> Vec<Vec<i64>> {
+    //define the size of each vector
+    let mut result = Vec::with_capacity(n);
+    let chunk_size = input.len() / n;
+    let remainder = input.len() % n;
+
+    let mut start = 0;
+    for i in 0..n {
+        // Calculate the current chunk size, adding 1 if within the remainder count
+        let end = start + chunk_size + if i < remainder { 1 } else { 0 };
+        result.push(input[start..end].to_vec());
+        start = end;
+    }
+
+    result
+}
+// given an unsorted array of some length, this function splits the array into pairs
+// and then merges the ordered pairs until we have one array
+fn sort(input: Vec<i64>) -> Vec<i64> {
+    //start by splitting the input array into arrays of size two,
+    let mut unsorted: Vec<Vec<i64>> = split_array(input.len(), input);
+    //now we want to sort merge each pair of arrays until we have one array
+    while unsorted.len() > 1 {
+        let mut pos = 0;
+        while pos < unsorted.len() - 1 {
+            //because of a borrow checker , I have to split the vector to obtain two mutuable referances of non overlapping
+            //slices
+            //I could do this unsafely which would, in my opinion be a neater implementation
+            let (one, two) = unsorted.split_at_mut(pos + 1);
+            //replace now merged vector
+            unsorted[pos] = merge(&mut one[one.len() - 1], &two[0]).to_vec();
+            unsorted.remove(pos + 1);
+            pos += 2;
+        }
+    }
+    unsorted[0].to_vec()
+}
+fn merge_results(mut unsorted: Vec<Vec<i64>>) -> Vec<i64> {
+    while unsorted.len() > 1 {
+        let mut pos = 0;
+        while pos < unsorted.len() - 1 {
+            //because of a borrow checker , I have to split the vector to obtain two mutuable referances of non overlapping
+            //slices
+            //I could do this unsafely which would, in my opinion be a neater implementation
+            let (one, two) = unsorted.split_at_mut(pos + 1);
+            //replace now merged vector
+            unsorted[pos] = merge(&mut one[one.len() - 1], &two[0]).to_vec();
+            unsorted.remove(pos + 1);
+            pos += 2;
+        }
+    }
+    unsorted[0].to_vec()
+}
+//given sorted vectors this merges them into one result vector
+fn merge<'a>(one: &'a mut Vec<i64>, two: &Vec<i64>) -> &'a Vec<i64> {
+    //iterate through each value in the second array to find it's place in the first
+    for second in two {
+        let mut pos = 0;
+        while pos < one.len() {
+            //we want to keep moving through the first array until the element in the first array is
+            //greater than the element in the second
+            if second <= &one[pos] {
+                //insert the element in the second array before this
+                one.insert(pos, *second);
+                break;
+            }
+            // if this is the final element of the first array add to the end
+            else if pos == one.len() - 1 && second > &one[pos] {
+                one.push(*second);
+                break;
+            } else {
+                pos += 1
+            }
+        }
+    }
+    one
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_split_array() {
+        let input = random_vec(10);
+        assert_eq!(split_array(2, input).len(), 2);
+    }
+    #[test]
+    fn test_merge_results() {
+        assert_eq!(
+            merge_results(vec![vec![5, 6], vec![3, 4], vec![1, 2]]),
+            vec![1, 2, 3, 4, 5, 6]
+        )
+    }
+    #[test]
+    fn test_merge() {
+        //test for a simple case of vec of len 2
+        assert_eq!(*merge(&mut vec![1, 4], &vec![2, 3]), vec![1, 2, 3, 4]);
+        //testing with 3 elements and having an element of the second array be the greatest
+        assert_eq!(
+            *merge(&mut vec![2, 6, 7], &vec![2, 3, 9]),
+            vec![2, 2, 3, 6, 7, 9]
+        );
+        //test where the second element contains the lowest value
+        assert_eq!(
+            *merge(&mut vec![3, 6, 7], &vec![2, 3, 9]),
+            vec![2, 3, 3, 6, 7, 9]
+        );
+    }
+    #[test]
+    fn test_sort() {
+        assert_eq!(sort(vec![2, 1, 3]), vec![1, 2, 3]);
+        assert_eq!(sort(vec![2, 1, 6, 3]), vec![1, 2, 3, 6]);
+    }
 }
--- assignments/05-concurrency/src/thread_pool.rs
+++ assignments/05-concurrency/src/thread_pool.rs
@@ -1,35 +1,42 @@
-use std::thread::{self, Thread,Builder};
 use crate::error::ThreadPoolError;
-
-
-
-pub struct ThreadPool {
-    builders: Vec<Builder>
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread::{self, JoinHandle};
+pub struct ThreadPool<T: Send + 'static, R: Send + Sized + 'static> {
+    workers: Vec<Worker<T, R>>,
+    sender: Sender<Job<T, R>>,
+    resultreciever: Receiver<R>,
 }
-
-impl ThreadPool {
+type Job<T, R> = Box<dyn FnOnce(T) -> R + Send + 'static>;
+impl<T: Send + Clone + 'static, R: Send + Clone + 'static> ThreadPool<T, R> {
     /// Create a new LocalThreadPool with num_threads threads.
     ///
     /// Errors:
     /// - If num_threads is 0, return an error
-    /// Build the thread by using Thread Build
-    pub fn new(num_threads: usize) -> Result<Self,ThreadPoolError>{
-       // check if the amount of desired threads is 0
-        let mut output = ThreadPool{
-            builders:vec![]
-        };
-        if num_threads == 0{
+    pub fn new(num_threads: usize, input_type: T, return_type: R) -> Result<Self, ThreadPoolError> {
+        if num_threads == 0 {
             Err(ThreadPoolError::ZeroThreads)
-        }else{
-            let mut i = 0;
-            let mut builders:Vec<Builder> = vec![];
-            while i < num_threads{
-                //create a new thread builder
-                builders.push(thread::Builder::new());
-                i +=1;
+        } else {
+            let mut workers: Vec<Worker<T, R>> = Vec::new();
+            let (tx, rx): (Sender<Job<T, R>>, Receiver<Job<T, R>>) = mpsc::channel();
+            let (result_sender, result_reciever) = mpsc::channel();
+            let rx = Arc::new(Mutex::new(rx));
+            for id in 0..num_threads {
+                //We could fail to create a new thread if we are out of system resources
+                workers.push(Worker::new(
+                    id,
+                    Arc::clone(&rx),
+                    input_type.clone(),
+                    return_type.clone(),
+                    result_sender.clone(),
+                )?);
             }
-            output.builders = builders;
-            Ok(output)
+            Ok(ThreadPool {
+                workers: workers,
+                sender: tx,
+                resultreciever: result_reciever,
+            })
         }
     }
 
@@ -37,28 +44,96 @@ impl ThreadPool {
     ///
     /// Errors:
     /// - If we fail to send a message, report an error
-    pub fn execute<F:Fn>(&self, f: F) {
-        //first we are going to define a channel for our threads to report their results
-        
-        //First we are going to spawn our built threads
-        for builder in &self.builders{
-            builder.spawn(f)
-        }
-        todo!()
+    pub fn execute<F>(&self, f: F) -> Result<(), ThreadPoolError>
+    where
+        F: FnOnce(T) -> R + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).map_err(|_| ThreadPoolError::Send)?;
+        Ok(())
     }
     /// Retrieve any results from the thread pool that have been computed
-    pub fn get_results(&self) {
-        todo!()
+    pub fn get_results(&self) -> Vec<R> {
+        let mut output = Vec::new();
+        for _ in 0..self.workers.len() {
+            if let Ok(result) = self.resultreciever.recv() {
+                output.push(result);
+            } else {
+                break; // Exit if there are no more messages or if an error occurs
+            }
+        }
+        output
+    }
+}
+#[allow(dead_code)]
+struct Worker<T, R> {
+    id: usize,
+    thread: JoinHandle<()>,
+    arg: T,
+    return_type: R,
+}
+impl<T: Send + Clone + 'static, R: Send + 'static> Worker<T, R> {
+    fn new(
+        id: usize,
+        rx: Arc<Mutex<Receiver<Job<T, R>>>>,
+        arg: T,
+        return_type: R,
+        result_sender: Sender<R>,
+    ) -> Result<Self, ThreadPoolError> {
+        let thread_arg = arg.clone();
+        let output = Worker {
+            id: id,
+            thread: thread::Builder::new()
+                .spawn(move || loop {
+                    {
+                        let job = rx.lock().unwrap().recv().unwrap();
+                        //preform function and get result
+                        let result = job(thread_arg.clone());
+                        let _ = result_sender.send(result);
+                    }
+                })
+                .map_err(|_| ThreadPoolError::ThreadOverload)?,
+            arg: arg.clone(),
+            return_type,
+        };
+        Ok(output)
     }
 }
 #[cfg(test)]
-mod test{
+mod test {
     use super::*;
     #[test]
-    fn test_new_threads(){
-        //check error case
-        assert_eq!(ThreadPool::new(0).is_err_and(|x| x==ThreadPoolError::ZeroThreads), true);
-        //check to see if we created the right number of thread builders
-        assert_eq!(ThreadPool::new(5).unwrap().builders.len(),5);
+    fn create_thread_pool_fail() {
+        //test if zero threads are made we get an error
+        assert!(
+            ThreadPool::new(0, 0, 0).is_err(),
+            "Expected the Threadpool to return an Erorr if constructed with zero threads"
+        )
+    }
+    #[test]
+    fn create_thread_pool_success() {
+        let pool = ThreadPool::<i32, i32>::new(4, 0, 0);
+        assert!(
+            pool.is_ok(),
+            "Expected the thread pool to be created successfully."
+        );
+    }
+
+    #[test]
+    fn execute_multiple_jobs_and_get_results() {
+        let pool = ThreadPool::<i32, i32>::new(4, 0, 0).expect("Failed to create thread pool");
+
+        // Schedule multiple jobs
+        for i in 0..4 {
+            pool.execute(move |x| x + i).expect("Failed to execute job");
+        }
+
+        // Collect results and verify all expected results are present
+        let results = pool.get_results();
+        assert_eq!(results.len(), 4, "Expected four results.");
+        assert!(results.contains(&0));
+        assert!(results.contains(&1));
+        assert!(results.contains(&2));
+        assert!(results.contains(&3));
     }
 }
